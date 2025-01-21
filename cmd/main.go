7ae56cee6127d4c50edccb902b1e9cb39587b901
hassan el ouazizi <ouazizi2code@gmail.com>
2025-01-21 16:53:57 +0100

    just remove the older handlers

--- cmd/main.go
+++ cmd/main.go
@@ -13,20 +13,14 @@ import (
 
 func init() {
 	database.Create_database()
+	handlers.ParseTemplates()
 }
 
 func main() {
 	// Get the current working directory
 	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/create_account", auth.Signup_treatment)
+	http.HandleFunc("/create_account", auth.Register)
 	http.HandleFunc("/log_in", auth.Log_in)
-	http.HandleFunc("/create_post", handlers.CreatePost)
-	http.HandleFunc("/filterPost", handlers.FilterPosts)
-	http.HandleFunc("/myposts", handlers.MyPosts)
-	http.HandleFunc("/likedposts", handlers.LikedPosts)
-	http.HandleFunc("/categorizePost", handlers.CategorizePosts)
-	http.HandleFunc("/settings", handlers.Settings)
-	http.HandleFunc("/logout", handlers.Logout)
 	http.HandleFunc("/static/", handlers.Serve_Static)
 	fmt.Println("server is running on port 8080 ... http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
