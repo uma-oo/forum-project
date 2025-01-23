f6425f55d5d03f5e08021b5c9fe7e7d4c043dbff
hassan el ouazizi <ouazizi2code@gmail.com>
2025-01-23 10:57:41 +0100

    add config and logger concept

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,8 @@ import (
 	"forum/internal/auth"
 	"forum/internal/database"
 	"forum/internal/handlers"
+	"forum/pkg/config"
+	"forum/pkg/logger"
 )
 
 func init() {
@@ -18,10 +20,23 @@ func init() {
 
 func main() {
 	// Get the current working directory
+	logger, err := logger.Create_Logger()
+	if err != nil {
+		fmt.Println("here")
+		log.Fatal(err)
+
+	}
+	defer logger.Close()
+
+	// lets load the configuration
+	configuration := config.LoadConfig()
+	fmt.Printf("Server starting on port: %d >>> http://localhost:8080\n", configuration.Port)
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/create_account", auth.Register)
 	http.HandleFunc("/log_in", auth.Log_in)
+	http.HandleFunc("/create_Post", handlers.Craete_post)
 	http.HandleFunc("/static/", handlers.Serve_Static)
-	fmt.Println("server is running on port 8080 ... http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//fmt.Println("server is running on port 8080 ... http://localhost:8080")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", configuration.Port), nil))
 }
