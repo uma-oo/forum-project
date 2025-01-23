719e2992fe6fedee34ceb9e33961371ec465aa7b
hassan el ouazizi <ouazizi2code@gmail.com>
2025-01-23 17:11:24 +0100

    modify the function by implementing the token in database

--- internal/auth/tokens.go
+++ internal/auth/tokens.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"fmt"
 	"net/http"
+
+	"forum/internal/database"
 )
 
 func IsCookieSet(r *http.Request, cookieName string) bool {
@@ -16,6 +18,15 @@ func IsCookieSet(r *http.Request, cookieName string) bool {
 		fmt.Println("Error retrieving cookie:", err)
 		return false
 	}
+	// lets extract the token value from the cookie and compare it with the one we have in databse
+	token := cookie.Value
+	var tokenExist bool
+	// lets extract the token from users table
+	// be care full with  no token
+	tokenErr := database.Database.QueryRow("SELECT EXISTS (SELECT 1 FROM users WHERE token = $1)", token).Scan(&tokenExist)
+	if tokenErr != nil || !tokenExist {
+		return false
+	}
 
-	return cookie.Value != ""
+	return true
 }
