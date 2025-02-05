5f768207b345f62f85b32feb897889f500af98a5
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-05 15:01:26 +0100

    add middleware logig

--- internal/middlewares/auth_middelware.go
+++ internal/middlewares/auth_middelware.go
@@ -2,26 +2,18 @@
 package middlewares
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+
+	"forum/internal/auth"
 )
 
 func Auth_Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// lets log the request
-		log.Printf("%v recieved a request >>   Method: %s, URL: %s, Client IP: %s", r.Header.Get("Date"), r.Method, r.URL.Path, r.RemoteAddr)
-		log.Print("------------------------")
-		// now lets check for authorisation
-
-		cookie, err := r.Cookie("token")
-		fmt.Printf("cookie: %v\n", cookie)
-		if err != nil {
-			fmt.Printf("err: %v\n", err)
+		// Check if the request contains a token
+		if !auth.IsCookieSet(r, "token") {
+			http.Redirect(w, r, "/login", http.StatusFound) // Use StatusFound (302)
+			return
 		}
-		fmt.Printf("cookie: %v\n", cookie)
-
-		// now call the next hamdel in the chain
 		next.ServeHTTP(w, r)
 	})
 }
