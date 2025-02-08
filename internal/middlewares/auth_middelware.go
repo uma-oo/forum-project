93834dc7521d538e0b721ea08fe6386484e0c49d
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-08 10:33:45 +0100

    some updates

--- internal/middlewares/auth_middelware.go
+++ internal/middlewares/auth_middelware.go
@@ -4,13 +4,13 @@ package middlewares
 import (
 	"net/http"
 
-	"forum/internal/auth"
+	"forum/internal/utils"
 )
 
 func Auth_Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Check if the request contains a token
-		if !auth.IsCookieSet(r, "token") {
+		if !utils.IsCookieSet(r, "token") {
 			http.Redirect(w, r, "/login", http.StatusFound) // Use StatusFound (302)
 			return
 		}
