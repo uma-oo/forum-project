188c50e4e993ff2a70b571552dd7dae19e27924e
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-08 13:35:49 +0100

    dont worry

--- cmd/main.go
+++ cmd/main.go
@@ -40,21 +40,20 @@ func main() {
 	http.Handle("/liked_posts", middlewares.Auth_Middleware(http.HandlerFunc(handlers.LikedPosts)))
 	http.HandleFunc("/login", handlers.Login)
 	http.HandleFunc("/register", handlers.Register)
-
 	// routes for auth handlers in auth package we need to add the auth middleware for login and register likly deferrant
-	//http.HandleFunc("/auth/register", auth.Register)
+	// http.HandleFunc("/auth/register", auth.Register)
 	http.Handle("/auth/register", middlewares.Reg_Log_Middleware(http.HandlerFunc(auth.Register)))
-	//http.HandleFunc("/auth/log_in", auth.LogIn)
+	// http.HandleFunc("/auth/log_in", auth.LogIn)
 	http.Handle("/auth/log_in", middlewares.Reg_Log_Middleware(http.HandlerFunc(auth.LogIn)))
 	http.HandleFunc("/auth/logout", auth.LogOut)
 
 	// routes for forms actions
-	// http.HandleFunc("/filter_posts", handlers.FilterPosts)
+	//http.HandleFunc("/filter_posts", handlers.FilterPosts)
 	http.Handle("/api/add_post", middlewares.Auth_Middleware(http.HandlerFunc(handlers.AddPost)))
 	http.Handle("/api/react_to_post", middlewares.Auth_Middleware(http.HandlerFunc(handlers.PostReactions)))
 	http.Handle("/api/add_post_comment", middlewares.Auth_Middleware(http.HandlerFunc(handlers.AddPostComment)))
 	http.Handle("/api/react_to_comment", middlewares.Auth_Middleware(http.HandlerFunc(handlers.LikeComment)))
-	
+
 	fmt.Printf("Server starting on port: %d >>> http://localhost:8080\n", configuration.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", configuration.Port), nil))
 }
