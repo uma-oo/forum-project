e77bc5d0fc9c75c958aadda43cd24a05c3fa425e
hassan el ouazizi <ouazizi2code@gmail.com>
2025-02-08 10:36:08 +0100

    add cookie function

--- internal/auth/log_out.go
+++ internal/auth/log_out.go
@@ -5,18 +5,19 @@ import (
 	"net/http"
 
 	"forum/internal/handlers"
+	"forum/internal/utils"
 )
 
 func LogOut(w http.ResponseWriter, r *http.Request) {
 	pages := handlers.Pagess
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		pages.All_Templates.ExecuteTemplate(w, "error.html", "Method Not Allowed")
+		pages.All_Templates.ExecuteTemplate(w, "error.html", "405 Method Not Allowed")
 		return
 	}
 
 	// lets check in first that is already have a session
-	if IsCookieSet(r, "token") {
+	if utils.IsCookieSet(r, "token") {
 		http.SetCookie(w, &http.Cookie{
 			Name:   "token", // name of the cookie
 			Value:  "",      // clear the cookie value
@@ -26,12 +27,11 @@ func LogOut(w http.ResponseWriter, r *http.Request) {
 			// Secure:   true,            // ensure cookie is only sent over HTTPS
 		})
 		log.Print("A User logged out")
-		http.Redirect(w, r, "/", http.StatusFound)
+
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		pages.All_Templates.ExecuteTemplate(w, "error.html", "Not Found")
 		return
 	}
 	http.Redirect(w, r, "/", http.StatusFound)
-	return
 }
