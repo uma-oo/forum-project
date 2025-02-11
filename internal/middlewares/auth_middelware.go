8ab1b06c0b24acd7173e415b03d9d254cd85f531
ihajji <hajji.ismail.851@gmail.com>
2025-02-11 11:22:26 +0100

    525825821249

--- internal/middlewares/auth_middelware.go
+++ internal/middlewares/auth_middelware.go
@@ -10,6 +10,7 @@ import (
 func Auth_Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Check if the request contains a token
+		
 		if !utils.IsCookieSet(r, "token") {
 			http.Redirect(w, r, "/login", http.StatusFound) // Use StatusFound (302)
 			return
